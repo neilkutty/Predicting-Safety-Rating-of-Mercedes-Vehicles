c83bbe12f12fa564017715df8a9231654cebb13d
sampsonsimpson <NNK@Neilss-MacBook-Pro.local>
2017-09-17 00:03:56 -0400

    begin nn

--- Mercedes.py
+++ Mercedes.py
@@ -142,8 +142,9 @@ nmerc_scaled = preprocessing.scale(nmerc)
 
 """
 #%%
-import os
-os.chdir('KerasPython')
+npmerc = nmerc.drop(['ID'],axis=1).as_matrix()
+X = npmerc[:,1:377]
+Y = npmerc[:,0]
 #%%
 from keras.models import Sequential
 from keras.layers import Dense
@@ -151,14 +152,14 @@ import numpy
 
 numpy.random.seed(9291)
 
-dataset = numpy.loadtxt('pima-indians-diabetes.csv', delimiter=',')
-X = dataset[:,0:8]
-Y = dataset[:,8]
+#dataset = numpy.loadtxt('pima-indians-diabetes.csv', delimiter=',')
+#X = dataset[:,0:8]
+#Y = dataset[:,8]
 #%%
 
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(12, input_dim=376, activation='sigmoid'))
+model.add(Dense(8, activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))
 #%%
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
